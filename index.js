9b9532abc7dd720597b3d9d2868ed952f51a57b9
saklainMostake <saklain15-12003@diu.edu.bd>
2022-11-08 12:49:59 +0600

    push

--- index.js
+++ index.js
@@ -15,11 +15,24 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ckrddue.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+
+async function run(){
+    try{
+        const foodCollection = client.db('homeFood').collection('services')
+        app.get('/foods', async(req, res) =>{
+           const query = {}
+           const cursor =  foodCollection.find(query)
+           const foods = await cursor.toArray()
+            res.send(foods)
+        })
+
+    }
+    finally{
+
+    }
+}
+run().catch(error => console.log(error))
 
 
 
